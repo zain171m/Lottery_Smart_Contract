74d8248121ddc00da22a72e6a09b6ad97ddab374
zain171m <zain@Zain>
2023-10-09 23:47:18 +0500

    First Commit

---
+++ Lottery.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract LotteryContract{
+    address public manager;
+    address payable[] public candidates;
+    address payable public winner;
+
+    constructor(){
+        manager = msg.sender;
+    }
+
+    receive() external payable {
+        require(msg.value == 0.000001 ether);
+        candidates.push(payable(msg.sender));
+    }
+
+    function getBalance() public view returns(uint){
+        require(msg.sender == manager);
+        return address(this).balance;
+    }
+
+    function getRandom() public view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, candidates.length)));
+    }
+
+    function PickWinner() public{
+        require((msg.sender == manager));
+        require(candidates.length >= 2);
+        uint r = getRandom();
+        uint index = r%candidates.length;
+        winner = candidates[index];
+        winner.transfer(getBalance());
+        candidates = new address payable[](0);
+    }
+}
